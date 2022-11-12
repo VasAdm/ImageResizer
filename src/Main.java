42f6da33fb68d9f2e672277646d1e59aa82a43e7
VasAdm <ershovvasiliy174@gmail.com>
2022-11-12 16:00:29 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Objects;
+
+public class Main {
+    private static int newWidth = 300;
+
+    public static void main(String[] args) {
+        String srcFolder = "";
+        String dstFolder = "";
+
+        File srcDir = new File(srcFolder);
+
+        long start = System.currentTimeMillis();
+
+        File[] files = srcDir.listFiles();
+
+
+        int middle = Objects.requireNonNull(files).length / 2;
+
+        File[] files1 = new File[middle];
+        System.arraycopy(files, 0, files1, 0, files1.length);
+        ImageResizer resizer1 = new ImageResizer(files1, newWidth, dstFolder, start);
+        resizer1.start();
+
+        File[] files2 = new File[files.length - middle];
+        System.arraycopy(files, middle, files2, 0, files2.length);
+        ImageResizer resizer2 = new ImageResizer(files2, newWidth, dstFolder, start);
+        resizer2.start();
+    }
+}
